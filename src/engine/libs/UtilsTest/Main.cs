e3c6f2ada97f919ff4372d2936be813a71e1829a
Andrius Bentkus <Andrius.Bentkus@rwth-aachen.de>
2010-09-10 14:29:14 +0200

    Add Quake2 BSP parser.

--- src/engine/libs/UtilsTest/Main.cs
+++ src/engine/libs/UtilsTest/Main.cs
@@ -20,6 +20,7 @@
 //     along with Iron.  If not, see <http://www.gnu.org/licenses/>.
 //
 using System;
+using System.IO;
 using Iron.WADUtils;
 using Iron.BSPUtils;
 
@@ -29,8 +30,74 @@ namespace Iron.WADUtilsConsole
 	{
 		public static void Main (string[] args)
 		{
-			// TODO: write 3D Engine
+			FileStream fs = File.OpenRead("de_dust.bsp");
+			Console.WriteLine ("Parsing bsp file: {0}\n", fs.Name);
+			BSPParser p = new BSPParser(fs);
+			Console.WriteLine ("Loading Directory table");
+			if (p.LoadDirectoryTables())
+			{
+				p.OnLoadEntities += delegate(string entities) { };
+				Console.WriteLine ("Loading Entities");
+				p.LoadEntities();
+				
+				p.OnLoadPlane += delegate(Plane plane) { };
+				Console.WriteLine ("Loading Planes");
+				p.LoadPlanes();
+				
+				p.OnLoadMipTexture += delegate(MipTexture texture) { };
+				if (p.LoadMipTextureOffsets())
+				{
+					Console.WriteLine ("Loading Mip Textures");
+					p.LoadMipTextures();
+				}
+				
+				p.OnLoadVertex += delegate(Vector3f vertex) { };
+				Console.WriteLine ("Loading Vertices");
+				p.LoadVertices();
+				
+				p.OnLoadBSPNode += delegate(BSPNode node) { };
+				Console.WriteLine ("Loading BSP nodes");
+				p.LoadBSPNodes();
+				
+				p.OnLoadFaceTextureInfo += delegate(FaceTextureInfo textureInfo) { };
+				Console.WriteLine ("Loading Surface Texture Info");
+				p.LoadFaceTextureInfo();
+				
+				p.OnLoadFace += delegate(Face face) { };
+				Console.WriteLine ("Loading Faces");
+				p.LoadFaces();
+				
+				p.OnLoadLightMap += delegate(byte lightmap) { };
+				Console.WriteLine ("Loading lightmaps");
+				p.LoadLightMaps();
+				
+				p.OnLoadClipNode += delegate(ClipNode node) { };
+				Console.WriteLine ("Loading ClipNode");
+				p.LoadClipNodes();
+				
+				p.OnLoadBSPLeaf += delegate(BSPLeaf leaf) { };
+				Console.WriteLine ("Loading BSP leaves");
+				p.LoadBSPLeaves();
+				
+				p.OnLoadFaceListElement += delegate(short face_index) { };
+				Console.WriteLine ("Loading Face index list");
+				p.LoadFaceList();
+				
+				p.OnLoadEdge += delegate(Edge edge) { };
+				Console.WriteLine ("Loading Edges");
+				p.LoadEdges();
+				
+				p.OnLoadEdgeListElement += delegate(short edge) { };
+				Console.WriteLine ("Loading Edge List");
+				p.LoadEdgeList();
+				
+				p.OnLoadModel += delegate(Model model) { };
+				Console.WriteLine ("Loading Models");
+				p.LoadModels();
+			}
+			
+			fs.Close();
+			fs.Dispose();
 		}
 	}
 }
-
